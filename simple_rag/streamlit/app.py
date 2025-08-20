30171276e88957fe93953cba40a0253fe97e6e93
baphometyr <baphometyr.black@gmail.com>
2025-08-20 13:11:51 -0600

    WIP: code improvements

--- simple_rag/streamlit/app.py
+++ simple_rag/streamlit/app.py
@@ -4,9 +4,31 @@ from simple_rag.pipeline import Pipeline
 
 import streamlit as st
 import PyPDF2
+import csv
+import io
 
 ASSISTANT_MESSAGE = "Hello! I'm here to answer your questions."
 
+
+# --- INIT VALUES ---
+if 'azure_api_key' not in st.session_state:
+    st.session_state.azure_api_key = ""
+if 'azure_endpoint' not in st.session_state:
+    st.session_state.azure_endpoint = ""
+if 'azure_api_version' not in st.session_state:
+    st.session_state.azure_api_version = ""
+if 'azure_deployment_name' not in st.session_state:
+    st.session_state.azure_deployment_name = ""
+
+if 'openai_api_key' not in st.session_state:
+    st.session_state.openai_api_key = ""
+if 'openai_model_name' not in st.session_state:
+    st.session_state.openai_model_name = ""
+
+if 'local_hf_token' not in st.session_state:
+    st.session_state.local_hf_token = ""
+if 'local_model_name' not in st.session_state:
+    st.session_state.local_model_name = ""
 
 # --- ONCHANGE FUNCIONS ---
 def on_collection_change():
@@ -16,6 +38,14 @@ def on_collection_change():
             st.session_state.collection, 
             embedding)
 
+def on_llm_change():
+    if st.session_state.choice_llm == "OpenAI":
+        configure_openai()
+    elif st.session_state.choice_llm == "Azure OpenAI":
+        configure_azureopenai()
+    elif st.session_state.choice_llm == "Local (HuggingFace)":
+        configure_local()
+
 # --- MODALS ---
 @st.dialog("Create your collection:", dismissible=False)
 def build_collection():
@@ -24,17 +54,54 @@ def build_collection():
     collection_name = st.text_input("Collection name", max_chars=50)
     collection_path = st.text_input("Collection path", "indices/", max_chars=100)
 
-    # Upload pdf files
-    uploaded_files = st.file_uploader("Choose a PDF files", accept_multiple_files=True)
-    for uploaded_file in uploaded_files:                
-        if uploaded_file.type != 'application/pdf':
-            st.error('Some files are not PDFs', icon="🚨")
-        else:
-            pdf_reader = PyPDF2.PdfReader(uploaded_file)
-            for page in pdf_reader.pages:
-                text = page.extract_text()
-                if text:
-                    texts.append(text)
+    # Extension file
+    extension = st.selectbox(
+        "Extension File",
+        ("PDF", "CSV"),
+        index=None,
+        placeholder="Select extension file to upload...",
+    )
+
+    if extension == 'PDF':
+        # Upload pdf files
+        uploaded_files = st.file_uploader("Choose a PDF files", accept_multiple_files=True)
+        for uploaded_file in uploaded_files:                
+            if uploaded_file.type != 'application/pdf':
+                st.error('Some files are not PDFs', icon="🚨")
+            else:
+                try:
+                    pdf_reader = PyPDF2.PdfReader(uploaded_file)
+                    for page in pdf_reader.pages:
+                        text = page.extract_text()
+                        if text:
+                            texts.append(text)
+                except:
+                    st.error(f"Error reading file {uploaded_file.name}, please check the contents or delete it.")
+
+        # Chunking type
+        opcions = ['Page', 'Recursive', 'Character']
+        st.segmented_control("Chunking Type", opcions, disabled=True)
+
+    if extension == 'CSV':
+        # Upload pdf files
+        col_name = st.text_input("Column name to extract")
+        uploaded_file = st.file_uploader("Choose a CSV file", accept_multiple_files=False, disabled = col_name=="")
+        
+        if uploaded_file:
+            if uploaded_file.type != 'text/csv':
+                st.error('File is not CSV', icon="🚨")
+            else:
+                # extract texts
+                try:
+                    stringio = io.StringIO(uploaded_file.getvalue().decode("utf-8"))
+                    reader = csv.DictReader(stringio)
+                    texts = [row[col_name] for row in reader]
+                except:
+                    st.error("Error reading CSV file, Please verify that the column exists in the file.")
+        
+        opcions = ['None', 'Recursive', 'Character']
+        st.segmented_control("Chunking Type", opcions, disabled=True)
+        
 
     # Advanced options
     with st.expander("Advanced options"):
@@ -79,14 +146,14 @@ def build_collection():
             
 @st.dialog("Configure OpenAI parameters:")
 def configure_openai():
-    api_key = st.text_input("API Key", type="password")
-    model_name = st.text_input("Model")
+    st.session_state.openai_api_key = st.text_input("API Key", value=st.session_state.openai_api_key, type="password")
+    st.session_state.openai_model_name = st.text_input("Model", value=st.session_state.openai_model_name)
 
     if st.button("Save"):
         try:
             st.session_state.model = OpenAIProvider(
-                api_key=api_key,
-                model_name=model_name)
+                api_key=st.session_state.openai_api_key,
+                model_name=st.session_state.openai_model_name)
             st.success("Model configured successfully!")
 
         except:
@@ -95,18 +162,18 @@ def configure_openai():
 
 @st.dialog("Configure AzureOpenAI parameters:")
 def configure_azureopenai():
-    api_key = st.text_input("API Key", type="password")
-    azure_endpoint = st.text_input("Azure Endpoint")
-    api_version = st.text_input("API Version")
-    deployment_name = st.text_input("Deployment Name")
+    st.session_state.azure_api_key = st.text_input("API Key", value=st.session_state.azure_api_key, type="password")
+    st.session_state.azure_endpoint = st.text_input("Azure Endpoint", value=st.session_state.azure_endpoint)
+    st.session_state.azure_api_version = st.text_input("API Version", value=st.session_state.azure_api_version)
+    st.session_state.azure_deployment_name = st.text_input("Deployment Name", value=st.session_state.azure_deployment_name)
 
     if st.button("Save"):
         try:
             st.session_state.model = AzureOpenAIProvider(
-                api_key = api_key,
-                azure_endpoint = azure_endpoint,
-                api_version = api_version,
-                deployment_name = deployment_name)
+                api_key = st.session_state.azure_api_key,
+                azure_endpoint = st.session_state.azure_endpoint,
+                api_version = st.session_state.azure_api_version,
+                deployment_name = st.session_state.azure_deployment_name)
             st.success("Model configured successfully!")
 
         except Exception as e:
@@ -117,26 +184,28 @@ def configure_azureopenai():
 
 @st.dialog("Configure Local model parameters:")
 def configure_local():
-    model_name = st.text_input("Model Name (Hugging Face)")
-    hf_token = st.text_input("Hugging Face Token (if necessary)", type="password")
+    st.info('This model is loaded locally, performance depends entirely on your computing resources.')
+
+    st.session_state.local_model_name = st.text_input("Model Name (Hugging Face)", value=st.session_state.local_model_name)
+    st.session_state.local_hf_token = st.text_input("Hugging Face Token (if necessary)", value=st.session_state.local_hf_token, type="password")
     
     if st.button("Save"):
         try:
             with st.spinner("Downloading and Load Local Model..."):
                 st.session_state.model = LocalModelProvider(
-                    model_name=model_name,
-                    hf_token=hf_token)
+                    model_name=st.session_state.local_model_name,
+                    hf_token=st.session_state.local_hf_token)
                 st.success("Model configured successfully!")
-
         except:
             st.error("Error configuring Local model. Please check your parameters.")
+    st.session_state.choice_llm = ""
 
 
 # --- SIDE BAR ---
 with st.sidebar:
-    st.header("Settings")
-
     # Collection
+    st.header("Collection:")
+
     st.selectbox(
         "Collection:",
         IndexBuilder.list_collections(),
@@ -148,25 +217,17 @@ with st.sidebar:
     
     if st.button("Create collection"):
         build_collection()
-
+    
     # LLM
+    st.header("LLM:")
     st.selectbox(
         "LLM:",
         ["OpenAI", "Azure OpenAI", "Local (HuggingFace)"],
-        key='llm',
         index=None,
+        key='choice_llm',
+        on_change=on_llm_change,
         placeholder="Select a LLM...", 
         label_visibility="collapsed")
-    
-    if st.session_state.llm == "OpenAI":
-        configure_openai()
-    elif st.session_state.llm == "Azure OpenAI":
-        configure_azureopenai()
-    elif st.session_state.llm == "Local (HuggingFace)":
-        configure_local()
-
-
-
     
 
 # --- MESSAGES ---
